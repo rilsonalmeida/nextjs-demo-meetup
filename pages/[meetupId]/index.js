b5fbc1fc5da1db3a75fcd7813b0f8fec179b9bb1
Rilson Almeida <rilson.almeida@gmail.com>
2021-04-01 23:16:51 -0300

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -36,7 +36,7 @@ export async function getStaticPaths() {
 
   return {
     paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() }})),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
